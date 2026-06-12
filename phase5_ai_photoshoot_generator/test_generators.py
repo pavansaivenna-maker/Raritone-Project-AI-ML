5616a24bb741b70830bb82de39cf8f2b9f846d7e
Hansika Kota <hansikakota2006@gmail.com>
2026-06-12 12:50:49 +0530

    Create test_generators.py

---
+++ phase5_ai_photoshoot_generator/test_generators.py
@@ -0,0 +1,11 @@
+def test_fashion():
+    print("Fashion Model Generator Tested")
+
+def test_product():
+    print("Product Photo Generator Tested")
+
+def test_lifestyle():
+    print("Lifestyle Image Generator Tested")
+
+def test_marketing():
+    print("Marketing Image Generator Tested")
